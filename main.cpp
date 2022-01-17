2372a254b9420d0a4354e1f1b745f4098d23593e
pjrje4 <58542025+pjrje4@users.noreply.github.com>
2022-01-16 21:51:00 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,25 +14,30 @@ using namespace std;
 int main() { //main
 	Node* first;
 	Node* last;
+	int index = 0;
 	while (true) { //loop
 		char input[20];
-		cout << "Enter a command (ADD, SEARCH, DELETE, QUIT): ";
+		cout << "Enter a command (ADD, PRINT, QUIT): ";
 		cin.getline(input, 19); //user input
 		if (strcmp(input, "ADD") == 0) { //add student and node
 			Student* s = new Student();
 			Node* current = new Node(s);
-			if (last != NULL) {
+			if (index != 0) {
 				last->setNext(current);
 			}
 			else {
-				Node* first = current;
-				Node* last = current;
-			}		
-
+				first = current;
+			}
+			last = current;
+			index++;
 		}
 		else if (strcmp(input, "PRINT") == 0) { //print nodes and students
-			if (first != NULL) {
+			cout << "printing" << endl;
+			if (index != 0) {
+				cout << "first not null" << endl;
 				Node* iter = first;
+				cout << "iter = first" << endl;
+
 				if (true) {
 					cout << iter->getStudent()->getFirst() << " " << iter->getStudent()->getLast() << " " << iter->getStudent()->getID() << " " << iter->getStudent()->getGPA() << endl; // output student info
 					if (iter->getNext() != NULL) {
